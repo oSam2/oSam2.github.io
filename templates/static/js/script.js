53515b3d43a85b766395624feae047036b45d4b3
Sam <samuelmarques96@live.com>
2021-06-14 15:34:18 -0300

    CSS FlexBox

---
+++ templates/static/js/script.js
@@ -0,0 +1,17 @@
+function taskBtn() {
+    var a = document.getElementsByClassName("task-btn");
+    console.log('Botão task')
+}
+
+  
+function homeBtn() {
+    document.getElementsByClassName("home-btn");
+
+    console.log('Botão home')
+}
+
+
+function backBtn() {
+    document.getElementsByClassName("back-btn");
+    console.log('Botão back')
+}
