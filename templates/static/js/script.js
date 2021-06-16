f5e6ce4fa054514627e56f0be80a727d768cc6c0
Sam <samuelmarques96@live.com>
2021-06-15 21:54:37 -0300

    More icons and clock

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -15,3 +15,15 @@ function backBtn() {
     document.getElementsByClassName("back-btn");
     console.log('Botão back')
 }
+
+
+var timeDisplay = document.getElementById("time");
+
+function refreshTime() {
+  var dateString = new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo", hour: '2-digit', minute: '2-digit'});
+  var formattedString = dateString.replace(", ", " - ", "date");
+  timeDisplay.innerHTML = formattedString;
+  
+}
+
+setInterval(refreshTime, 1000);
